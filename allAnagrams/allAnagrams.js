5e96e69101dfde26f24cdb076aee3d97d9e796ce
Olive Iosello <oiosello@gmail.com>
2020-09-10 09:53:45 -0700

    Worked through some of allAnagrams logic and coded a little to try and determine best path

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,51 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
+  // in str
+  // out arr
+  // consider repeated strings okay
+  //          length of str unknown, number of loops unknown
+  //          ideas: recursive??, helper func
+  // edge
+
+  // create result arr
+  // iterate over string
+  // create pointer var
+  // iterate over string second time
+  //   compare pointer to all other chars
+  //     if not equal push to result
+  // return result
+
+  function numOfLoops(string) {
+    let num = string.length
+    for (var i = 0; i < num; i++) {
+      for (var j = 0; j < string.length; j++) {
+        
+      }
+    }
+  }
+
+  // create helper func to determine num of times to loop
+  // set var to length
+  //   for num of chars in str
+  //     loop over each char
+
+  // consider: what to return in helper vs main func
+  //          
+
+
 var allAnagrams = function(string) {
-  // Your code here.
+  let result = [];
+  for (var i = 0; i < string.length; i++) {
+    let pointer = string[i];
+    for (var j = 0; j < string.length; j++) {
+      let letter = string[j]
+      // if (i !== j) {
+        result.push(pointer + letter);
+      // }
+      // only yeilds combos for two 
+    }
+  }
+  return result;
+};
 };
